dffa10b27b238349e10b3cbe38b5f92b7d4df0eb
Emmaobi7 <emma46574@gmail.com>
2022-09-08 15:05:01 -0700

    size of data types in 32 and 64 it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+i
 /**
  * main- entrypoint
  * Description: sizes of the data types
@@ -7,12 +7,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s) \n", sizeof(char));
-	printf("Size of an int: %lu byte(s) \n", sizeof(int));
-	printf("Size of a long int: %lu byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s) \n", sizeof(float));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
 
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
-
 }
