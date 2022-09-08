d893ffdc6c457febc2fa693cb622e1b332ceb953
Emmaobi7 <emma46574@gmail.com>
2022-09-08 11:23:44 -0700

    display using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- entrypoint
+ * Description: Display text
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
