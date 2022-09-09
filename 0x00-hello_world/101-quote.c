64a73966fb7b5deaea3fb31c942dfa995892ee94
Emmaobi7 <emma46574@gmail.com>
2022-09-08 18:01:33 -0700

    last changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main- entrypoint
+ * Description: write to stderr
+ * Return: returns 1 for error
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
