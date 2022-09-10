d40d22fee19944e66b009ae5713d45d1e57aa455
Emmaobi7 <emma46574@gmail.com>
2022-09-09 18:10:12 -0700

    hmm

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,13 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			
-			if (i == 9 && j== 9)
+			if (i == 8 && j == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
