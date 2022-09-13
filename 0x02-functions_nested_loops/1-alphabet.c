b1561ffda69e5fa3942ab11366cef7f8a4a2095b
Emmaobi7 <emma46574@gmail.com>
2022-09-13 07:00:53 -0700

    still testing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,20 +9,13 @@
 
 
 void print_alphabet(void);
-
-
-int main(void)
-{
-
-	_putchar();
-
-	return (0);
-}
-
-void print_alphabet(void)
 {
 	int i;
-	for (i='a'; i<= 'z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 }
+
