b59e23160e24a6ad0dacc528f87f3854fe4ab593
Emmaobi7 <emma46574@gmail.com>
2022-09-13 18:01:37 -0700

    testing

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main- entrypoint
+ * Description: program to compute and add mutltiples of int
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i, n = 10;
+	int x = 5;
+	int y = 3;
+	int res = 0;
+
+	for (i = 1; i < n; i++)
+	{
+		if ((i % x == 0) || (i % y == 0))
+		{
+			res += 1;
+		}
+		printf("%d\n", res);
+	
+	}
+	return (0);
+}
+
+
+
+
