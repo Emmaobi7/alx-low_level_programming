abf13ae2c87bf1421b90f620c2aa06b5c9efa2a9
Emmaobi7 <emma46574@gmail.com>
2022-09-19 15:10:19 -0700

    final commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,38 @@
 #include "main.h"
 
 /**
- * _atoi- converts chars to ints
- * @s: pointer to char to be converted
- * Return: 0
+ * _atoi - Find and print the first number in a string as an int
+ * @s: String to convert to int
+ *
+ * Return: The int
  */
-
-
-
-
-
-
-
-
 int _atoi(char *s)
 {
-	int sign = 1;
-	int num = 0;
+	int i, n, m;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-		else if (num > 0)
-			break;
-	} while (*s++);
-
-	return (num * sign);
+	i = n = m = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == '-')
+			m++;
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			while (*(s + i) >= '0' && *(s + i) <= '9')
+			{
+				if (n == 0)
+				{
+					n = (n * 10) + (*(s + i) - '0');
+					n *= -1;
+				}
+				else
+					n = (n * 10) - (*(s + i) - '0');
+				i++;
+			}
+			if (m % 2 == 0)
+				n *= -1;
+			return (n);
+		}
+		i++;
+	}
+	return (0);
 }
