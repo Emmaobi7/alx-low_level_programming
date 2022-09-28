2f065a2f0ec6674dd321e1858c48e30b7ea9d5a4
Emmaobi7 <emma46574@gmail.com>
2022-09-28 14:49:46 -0700

    some morw debugging

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,7 +17,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y == 0)
+	else if (y == 0)
 		return (1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
 	return (x * _pow_recursion(x, y - 1));
 }
