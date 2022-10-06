c903ab43671d28038f1b14badd16756f18e97f25
Emmaobi7 <emma46574@gmail.com>
2022-10-06 15:54:41 +0100

    more debugging

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		j++;
 	}
-	p = malloc(sizeof(*s1) * (i * n));
+	p = malloc(sizeof(*s1) * (i + n + 1));
 	if (p == NULL)
 		return (NULL);
 	k = 0;
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,35 +1,28 @@
 #include <stdlib.h>
-#include "main.h"
-
+#include <stdio.h>
 /**
- * array_range- creates an array of integers
- * @min: first value of array
- * @max: last value of array
- * Return: pointer to new array
+ * array_range - Create array of integers filled with numbers from min to max
+ * @min: First number in the array
+ * @max: Largest number in the array
+ *
+ * Return: Pointer to new array, NULL if failed
  */
-
-
-
-
-
-
 int *array_range(int min, int max)
 {
-	int *p;
-	int size, i;
+	int *ar;
+	int i, size, n;
 
 	if (min > max)
 		return (NULL);
 	size = max - min;
-
-	p = malloc(sizeof(int) * (size + 1));
-
-	if (p == NULL)
-		return (NULL);
-	for (i = min; i <= max; i++)
+	ar = malloc((size + 1) * sizeof(int));
+	i = 0;
+	n = min;
+	while (n <= max)
 	{
-		p[i] = i;
+		ar[i] = n;
+		i++;
+		n++;
 	}
-	return (p);
-
+	return (ar);
 }
