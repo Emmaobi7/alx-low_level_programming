139e4e554b05afaa3b9a881ff68f35b89ccd4145
Emmaobi7 <emma46574@gmail.com>
2022-10-12 10:38:12 -0700

    more debugging

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,8 +18,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,10 +18,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
+			if (cmp(array[i]))
 			return (i);
 		}
 	}
