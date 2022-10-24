7c2285ad21bf67e7c3beae48fc120c0a30f720f6
Emmaobi7 <emma46574@gmail.com>
2022-10-24 11:52:45 -0700

    im making the mistake

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,17 +14,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *f = *head;
 	listint_t *tmp;
 
 	if (head == NULL)
 		return;
 
-	while (f)
+	while (*head)
 	{
-		tmp = f->next;
-		free(f);
-		f = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+
 	head = NULL;
 }
